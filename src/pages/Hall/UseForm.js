93fd189c56111c3d815e8222282af671dda25cb8
Aline Andrade <marson.aline@gmail.com>
2021-09-21 16:49:18 -0300

    ajustando pra pagina voltar a rodar

--- src/pages/Hall/UseForm.js
+++ src/pages/Hall/UseForm.js
@@ -1,31 +1,31 @@
-import { useState } from "react";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
-import { useHistory } from 'react-router-dom';
+// import { useState } from "react";
+// import { unstable_renderSubtreeIntoContainer } from "react-dom";
+// import { useHistory } from 'react-router-dom';
 
 
-const useForm = () => {
-    const history = useHistory()
+// const useForm = () => {
+//     const history = useHistory()
     
-    function navigateToMenuMain() {
-        history.push('/menumain')
-    }
+//     function navigateToMenuMain() {
+//         history.push('/menumain')
+//     }
 
-    function navigateToMenuMorning() {
-            history.push('/menumorning') 
-    }
+//     function navigateToMenuMorning() {
+//             history.push('/menumorning') 
+//     }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        setErrors();
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         setErrors();
 
-        if(selecionou uma mesa $$ apertou botao menumorning && anotou o nome do cliente){
-            navigateToMenuMorning()
-        }
-        if (selecionou uma mesa && anotou o nome do cliente && apertou botao menumain) {
-            navigateToMenuMain()
-        }
+//         if(selecionou uma mesa $$ apertou botao menumorning && anotou o nome do cliente){
+//             navigateToMenuMorning()
+//         }
+//         if (selecionou uma mesa && anotou o nome do cliente && apertou botao menumain) {
+//             navigateToMenuMain()
+//         }
        
-    }
+//     }
 
     // const logout 
     // essa parte não é da historia de usuario mas é util por enquanto
